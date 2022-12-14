bb5ae19d4101ca0dcc3e9f5a8ae6b393526d58b3
danitrancozo <49005105+danieltrancozo@users.noreply.github.com>
2022-12-14 23:11:35 +0000

    test with functions on json

---
+++ main.js
@@ -0,0 +1,14 @@
+let map = JSON.parse("./json-files/map.json");
+let componentFunction = JSON.parse(map.components.function);
+let componentModel = JSON.parse(map.components.model);
+let componentConstructor = new componentModel;
+let componentRender = new Function(componentFunction.function.arguments, componentFunction.function.body);
+
+onload(onloadFunctions());
+
+function onloadFunctions(){
+    componentConstructor.tag = "h1";
+    componentConstructor.class = null;
+    componentConstructor.id = "hello_world"
+    componentConstructor.style = "color: red; backgroundColor: yellow;"
+}
