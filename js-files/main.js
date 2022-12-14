7a559e60c007bfdf9d5727816963122fb7c00c71
danitrancozo <49005105+danieltrancozo@users.noreply.github.com>
2022-12-14 11:57:33 +0000

    same folder test

--- js-files/main.js
+++ js-files/main.js
@@ -1,4 +1,4 @@
-import {render} from "./functions/render.mjs"
+import {render} from "./render.mjs"
 let map = JSON.parse("./json-files/map.json");
 let Render = render;
 let components = JSON.parse(map.component);
