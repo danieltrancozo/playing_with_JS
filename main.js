500565ac6b64622b156d77789bd983088c4a536d
danitrancozo <49005105+danieltrancozo@users.noreply.github.com>
2022-12-15 01:36:39 +0000

    test

--- main.js
+++ main.js
@@ -1,15 +1,34 @@
-let map = JSON.parse("playing_with_JS/json-files/map.json");
-let componentFunction = JSON.parse(map.components.function);
-let componentModel = JSON.parse(map.components.model);
-let componentConstructor = new componentModel;
-let componentRender = new Function(componentFunction.function.arguments, componentFunction.function.body);
+let res;
+let mapadress = "https://danieltrancozo.github.io/playing_with_JS/json-files/map.json";
+let map;
+let componentFunction;
+let componentModel;
 
-onload(onloadFunctions());
-
-function onloadFunctions(){
-    componentConstructor.tag = "h1";
-    componentConstructor.class = null;
-    componentConstructor.id = "hello_world";
-    componentConstructor.style = "color: red; backgroundColor: yellow;";
-    componentRender("root",componentConstructor);
+function requests(adress){
+    let request = new XMLHttpRequest();
+    request.open('GET', adress, true);
+    request.onload = function(){
+        res = JSON.parse(request.responseText);
+        console.log(res);
+    }
+    request.send();
 }
+
+function main(){
+    requests(mapadress);
+    map = res;
+    requests(map[0].components.model);
+    componentModel = res[0];
+    requests(map[0].components.function);
+    componentFunction = new Function(res[0].function.arguments, res[0].function.body)
+    start();
+}
+
+function start(){
+    componentModel.tag = "h1";
+    componentModel.class = null;
+    componentModel.id = "hello_world"
+    componentModel.style = "color: red; backgroundColor: yellow;"
+}
+
+onload(main());
